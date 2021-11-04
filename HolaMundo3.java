186d228f97ae98b8574e1bacf9ec613fa467c318
Mirrey Andy <villanueva.guzman.andy@gmail.com>
2021-11-04 15:11:49 -0600

    modificacion al programa HolaMundo3.java

--- HolaMundo3.java
+++ HolaMundo3.java
@@ -3,6 +3,7 @@ public class HolaMundo3
         public  void  main ( String  a [])
         {
                 System.out.println ( " hola mundo2 " );
-		System.out.println ( " hola Lalo " );	
+		System.out.println ( " hola Lalo " );
+		System.out.println ( " hola Andy " );
         }
 }
