73a1528132f32b5ac6a7a27a5304162d576267db
LustrilloMendozaEduardoArturo <Lustrillomendozaeduardoarturo@gmail.com>
2021-11-04 11:53:04 -0600

    Ya alv

---
+++ HolaMundo3.java
@@ -0,0 +1,8 @@
+public class HolaMundo3
+{
+        public  void  main ( String  a [])
+        {
+                System.out.println ( " hola mundo2 " );
+		System.out.println ( " hola Lalo " );	
+        }
+}
